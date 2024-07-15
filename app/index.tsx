b8fca80cb7a2480e1fb90e584fd8831ed873f3e6
Oskar Kaczmarzyk <oskar.kaczmarzyk@icloud.com>
2024-07-15 14:47:09 +0200

    test: missing files

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from 'expo-router'
 
 export default function MainIndex() {
-	// return <Redirect href="/(tabs)/Home" />;
-	return <Redirect href="/Event" />
+	return <Redirect href="/(tabs)/Home" />
+	// return <Redirect href="/Event" />
 	// return <View style={{ backgroundColor: "red", flex: 1 }}></View>;
 }
