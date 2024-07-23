713ed0d1013bce23068a0ea3682d8239956aaea9
Oskar Kaczmarzyk <oskar@kaczmarzyk.dev>
2024-07-23 18:27:22 +0200

    chore: added tamagui (#32)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,32 @@
+import { tamaguiConfig } from '../tamagui.config'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { TamaguiProvider } from 'tamagui'
+import { useColorScheme } from 'react-native'
 import { Stack } from 'expo-router'
 
 export default function TabsLayout() {
+	const colorScheme = useColorScheme()
+	const [loaded] = useFonts({
+		Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+		InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf')
+	})
+
+	if (!loaded) return null
+
 	return (
-		<Stack screenOptions={{ headerShown: false }}>
-			<Stack.Screen
-				name="(tabs)"
-				options={{ headerShown: false }}
-			/>
-		</Stack>
+		<TamaguiProvider
+			config={tamaguiConfig}
+			defaultTheme={colorScheme!}
+		>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen
+						name="(tabs)"
+						options={{ headerShown: false }}
+					/>
+				</Stack>
+			</ThemeProvider>
+		</TamaguiProvider>
 	)
 }
--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,17 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = (() => {
 	/** @type {import('expo/metro-config').MetroConfig} */
-	const config = getDefaultConfig(__dirname)
+	const config = getDefaultConfig(__dirname, {
+		// [Web-only]: Enables CSS support in Metro.
+		isCSSEnabled: true
+	})
+
+	const { withTamagui } = require('@tamagui/metro-plugin')
+	module.exports = withTamagui(config, {
+		components: ['tamagui'],
+		config: './tamagui.config.ts',
+		outputCSS: './tamagui-web.css'
+	})
 
 	const { transformer, resolver } = config
 
