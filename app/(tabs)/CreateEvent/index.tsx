c741ddae45d5265d93194bb04c5500b394490fb4
Oskar Kaczmarzyk <oskar@kaczmarzyk.dev>
2024-07-23 18:14:28 +0200

    chore: PROPOSAL new pages files structure (#30)

--- app/(tabs)/CreateEvent/index.tsx
+++ app/(tabs)/CreateEvent/index.tsx
@@ -1,11 +1,1 @@
-import { Text, View } from 'react-native'
-import { Stack } from 'expo-router'
-
-export default function CreateEvent() {
-	return (
-		<View>
-			<Stack.Screen options={{ headerShown: true, title: 'Create Event' }} />
-			<Text>Fookin CreateEvent page innit</Text>
-		</View>
-	)
-}
+export { default } from './CreateEventContent'
