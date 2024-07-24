367f257af564fd4e1a3250862f9358f4c3ca8135
Oskar Kaczmarzyk <oskar@kaczmarzyk.dev>
2024-07-24 16:06:16 +0200

    chore: HomeScreen updates

---
+++ src/components/organisms/HomeScreen/HomeScreen.test.tsx
@@ -0,0 +1,16 @@
+import { HomeScreen } from './HomeScreen'
+import { renderRouter, screen } from 'expo-router/testing-library'
+
+describe('HomeScreen', () => {
+	it('renders component', () => {
+		renderRouter(
+			{
+				'/(tabs)/home': HomeScreen
+			},
+			{
+				initialUrl: '/(tabs)/home'
+			}
+		)
+		expect(screen.getByText('Create project screen')).toBeOnTheScreen()
+	})
+})
